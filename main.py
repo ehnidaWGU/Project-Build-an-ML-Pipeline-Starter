459fd67ddf76bf8a91ee25880058ad669eca9752
ehnidaWGU <ehnida@wgu.edu>
2025-12-18 19:35:44 -0500

    Fixed main.py basic_cleaning indentation error

--- main.py
+++ main.py
@@ -50,18 +50,18 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-        _ = mlflow.run(
-            f"{config['main']['components_repository']}/basic_cleaning",
-            "main",
-            env_manager="conda",
-            parameters={
-                "input_artifact": "sample.csv:latest",
-                "output_artifact": "clean_sample.csv",
-                "output_type": "clean_data",
-                "output_description": "Data after basic cleaning",
-                "min_price": config["etl"]["min_price"],
-                "max_price": config["etl"]["max_price"],
-            },
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/basic_cleaning",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_data",
+                    "output_description": "Data after basic cleaning",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"],
+                },
             )
 
         if "data_check" in active_steps:
